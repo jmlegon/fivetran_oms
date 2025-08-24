75bf604fec5677fca847d074e7058e2709336f35
jmlegon <jmlegon@MacBook-Pro-de-jmlegon.local>
2025-08-24 22:11:30 +0200

    Supprimer des Foreign Keys

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,21 +1,34 @@
-with base as (
-    select * from {{ ref('stg_customers') }}
-),
-scd2 as (
+with ranked as (
     select
-        customer_id,
-        last_name,
-        first_name,
-        gender,
-        email,
-        address,
-        zipcode,
-        city,
-        birthdate,
-        synced_at as start_date,
-        lead(synced_at) over (partition by customer_id order by synced_at) as end_date,
-        case when lead(synced_at) over (partition by customer_id order by synced_at) is null then true else false end as is_current
-    from base
-    where is_deleted = false
+        CUSTOMER_ID_C as customer_id_source,
+        CUST_LAST_NAME_C,
+        CUST_FIRST_NAME_C,
+        GENDER_C,
+        EMAIL_C,
+        ADDRESS_C,
+        ZIPCODE_C,
+        CITY_C,
+        CUST_BIRTHDATE_C,
+        _FIVETRAN_SYNCED,
+        row_number() over (
+            partition by CUSTOMER_ID_C
+            order by _FIVETRAN_SYNCED
+        ) as version_num
+    from {{ ref('stg_customers') }}
 )
-select * from scd2
+select
+    {{ dbt_utils.generate_surrogate_key(['customer_id_source','version_num']) }} as customer_key,
+    customer_id_source,
+    cust_last_name_c,
+    cust_first_name_c,
+    gender_c,
+    email_c,
+    address_c,
+    zipcode_c,
+    city_c,
+    cust_birthdate_c,
+    version_num,
+    current_timestamp as start_date,
+    null as end_date,
+    true as is_current
+from ranked

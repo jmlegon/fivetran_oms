dc94b4c5b89b71fe4886145af9b978086668bf22
jmlegon <jmlegon@MacBook-Pro-de-jmlegon.local>
2025-08-25 10:37:12 +0200

    Fix sources (POSTGRES_PUBLIC/SALESFORCE) + stg_orders ref

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,1 +1,44 @@
-select * from {{ source('postgres','ORDERS_FACT') }}
+{{ config(materialized='view') }}
+
+with oh as (
+    select
+        ORDER_ID,
+        CUSTOMER_ID,
+        ORDER_DATE,
+        SHIP_DATE,
+        ORDER_AMT,
+        ORDER_COST,
+        QTY_SOLD,
+        FREIGHT,
+        PYMT_TYPE,
+        GROSS_DOLLAR_SALES
+    from {{ source('postgres_public', 'ORDER_HEADER') }}
+),
+od as (
+    select
+        ORDER_ID,
+        ITEM_ID,
+        PROMOTION_ID,
+        QTY_SOLD,
+        UNIT_PRICE,
+        UNIT_COST,
+        DISCOUNT,
+        ORDER_DATE
+    from {{ source('postgres_public', 'ORDER_DETAIL') }}
+)
+
+select
+    od.ORDER_ID,
+    od.ITEM_ID,
+    oh.CUSTOMER_ID,
+    od.QTY_SOLD,
+    od.UNIT_PRICE,
+    od.UNIT_COST,
+    od.DISCOUNT,
+    oh.ORDER_DATE,
+    oh.SHIP_DATE,
+    oh.PYMT_TYPE,
+    oh.GROSS_DOLLAR_SALES,
+    oh.FREIGHT
+from od
+left join oh using (ORDER_ID)

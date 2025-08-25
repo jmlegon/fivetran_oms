0f9c8f43a3b328e2c0c3045760275d1fcec98694
jmlegon <jmlegon@MacBook-Pro-de-jmlegon.local>
2025-08-25 14:29:25 +0200

    Correction de dim_customers.sql supp _c

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -6,36 +6,36 @@
 
 with ranked as (
     select
-        CUSTOMER_ID_C as customer_id_source,
-        CUST_LAST_NAME_C,
-        CUST_FIRST_NAME_C,
-        GENDER_C,
-        EMAIL_C,
-        ADDRESS_C,
-        ZIPCODE_C,
-        CITY_C,
-        CUST_BIRTHDATE_C,
+        CUSTOMER_ID as customer_id_source,
+        CUST_LAST_NAME,
+        CUST_FIRST_NAME,
+        GENDER,
+        EMAIL,
+        ADDRESS,
+        ZIPCODE,
+        CITY,
+        CUST_BIRTHDATE,
         _FIVETRAN_SYNCED,
         row_number() over (
-            partition by CUSTOMER_ID_C
+            partition by CUSTOMER_ID
             order by _FIVETRAN_SYNCED
         ) as version_num
-    from {{ ref('stg_customers') }}
+    from {{ ref('stgustomers') }}
 )
 
 select
     {{ dbt_utils.generate_surrogate_key(['customer_id_source','version_num']) }} as customer_key,
     customer_id_source,
-    cust_last_name_c,
-    cust_first_name_c,
-    gender_c,
-    email_c,
-    address_c,
-    zipcode_c,
-    city_c,
-    cust_birthdate_c,
+    cust_last_name,
+    cust_first_name,
+    gender,
+    email,
+    address,
+    zipcode,
+    city,
+    cust_birthdate,
     version_num,
     current_timestamp as start_date,
     null as end_date,
-    true as is_current
+    true as isurrent
 from ranked
